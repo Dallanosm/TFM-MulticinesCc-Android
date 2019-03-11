2d3a40086338951a9b1642cd226501c4691dc56f
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2019-03-11 18:25:24 +0100

    Solved android x problems

--- app/src/main/java/com/multicinescc/app/view/adapter/RootAdapter.kt
+++ app/src/main/java/com/multicinescc/app/view/adapter/RootAdapter.kt
@@ -1,9 +1,9 @@
 package com.multicinescc.app.view.adapter
 
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * RootAdapter
@@ -11,7 +11,7 @@ import android.view.ViewGroup
 abstract class RootAdapter<T>(protected val items: MutableList<T> = mutableListOf(),
                               private val onItemClickListener: (T) -> Unit = {},
                               private val onLongClickListener: (T) -> Unit = {}
-) : androidx.recyclerview.widget.RecyclerView.Adapter<RootAdapter.RootViewHolder<T>>() {
+) : RecyclerView.Adapter<RootAdapter.RootViewHolder<T>>() {
 
     abstract val itemLayoutId: Int
 
